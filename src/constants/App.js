fd53c7a5a616451c355f0bc231ccde01c964e04e
elpoto <1258870+elbotho@users.noreply.github.com>
2018-12-12 14:50:17 +0100

    new(Search): increase search delay

--- src/constants/App.js
+++ src/constants/App.js
@@ -3,5 +3,5 @@ import LICENSES from "./Licenses"
 module.exports = {
   NEW_ENTRY_LICENSE: LICENSES.CC0,
   TILEHOSTING_API_KEY: "HOG1gcOUF2mFfwTfD5zS",
-  SEARCH_DELAY: 500
+  SEARCH_DELAY: 1000
 }
